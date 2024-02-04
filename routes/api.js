f4ae43fc7612a9a552799841a4e2d5e549333bf6
0Clark30 <austinlaw.clark@outlook.com>
2024-02-04 15:42:49 -0600

    npm uuid

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,10 @@
-const router = require('express').Router();
-const path = require('path');
+const router = require("express").Router();
+const path = require("path");
+const fs = require("fs");
+get {v4 : uuidv4} = require('uuid')
 
+router.get("/api/notes", async(req, res) => {
+    
+});
+
+router.post("/api/notes", async(req, res));
