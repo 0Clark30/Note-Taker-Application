c3790fca299eee45e7652dc8737b6182a517cdfa
0Clark30 <austinlaw.clark@outlook.com>
2024-02-04 16:30:37 -0600

    fixed launch bug

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,33 @@
 const router = require("express").Router();
-const path = require("path");
 const fs = require("fs");
-get {v4 : uuidv4} = require('uuid')
-
-router.get("/api/notes", async(req, res) => {
-    
+const { v4: uuidv4 } = require("uuid");
+router.get("/api/notes", async (req, res) => {
+  try {
+    const db = await JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+    res.json(db);
+  } catch (error) {
+    console.error("Error reading notes:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-router.post("/api/notes", async(req, res));
+router.post("/api/notes", (req, res) => {
+  try {
+    const db = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+    const newNote = {
+        id: uuidv4(),
+      title: req.body.title,
+      text: req.body.text,
+    };
+    db.push(newNote);
+    fs.writeFileSync("db/db.json", JSON.stringify(db, null, 2));
+    res.json(newNote);
+  } catch (error) {
+    console.error("Error adding note:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs")
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 const htmlRoutes = require('./routes/html')
+const api = require('./routes/api')
 const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
@@ -13,19 +14,12 @@ app.use(express.static("public"));
 
 // GET Route for notes html
 app.use(htmlRoutes);
-
-
-
-
-
-
-
-
+app.use(api);
 
 app.get("*", (req, res) =>
 res.sendFile(path.join(__dirname, "/public/index.html"))
 )
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT}`)
+  console.log(`App listening at ${PORT}`)
 );
